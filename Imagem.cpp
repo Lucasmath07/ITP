5a50a1768fc76ffc6cc01c2b6c8a9cbca402e53f
Lucasmath07 <lucas.mat.ferr.roch@gmail.com>
2025-07-09 23:05:45 -0300

    Update Imagem.cpp

--- Imagem.cpp
+++ Imagem.cpp
@@ -1,22 +1,53 @@
-#include <iostream>
-#include "paleta.cpp"
-#include "cor.h";
+class Imagem {
+private:
+    int Largura, Altura;
+    Cor pixel[1][3]; // imagem 1x3 fixa
 
-using namespace std;
+public:
+    // Construtor fixo para imagem 1x3
+    Imagem() : Largura(3), Altura(1) {
+        for (int i = 0; i < Altura; i++) {
+            for (int j = 0; j < Largura; j++) {
+                pixel[i][j] = Cor(0, 0, 0); // preto por padrão
+            }
+        }
+    }
 
-class Imagem{
+    // Define um pixel
+    void setPixel(int linha, int coluna, Cor cor) {
+        if (linha == 0 && coluna >= 0 && coluna < 3) {
+            pixel[linha][coluna] = cor;
+        }
+    }
 
-    private:
-    int const Largura, Altura;
-    Cor pixel[Altura][Largura];
-    public:
-    Imagem(){
-        Largura = 0;
-        Altura = 0;
-        pixel();
+    // Retorna um pixel
+    Cor getPixel(int linha, int coluna) {
+        if (linha == 0 && coluna >= 0 && coluna < 3) {
+            return pixel[linha][coluna];
+        }
+        return Cor();
+    }
+
+    // Salvar no formato PPM
+    void salvar(string nomeArquivo) {
+        ofstream arquivo(nomeArquivo);
+        if (!arquivo.is_open()) {
+            cout << "Erro ao4245 abrir o arquivo para salvar\n";
+            return;
+        }
+
+        arquivo << "P3\n";
+        arquivo << Largura << " " << Altura << "\n";
+        arquivo << "255\n";
+
+        for (int i = 0; i < Altura; ++i) {
+            for (int j = 0; j < Largura; ++j) {
+                Cor c = pixel[i][j];
+                arquivo << c.getR() << " " << c.getG() << " " << c.getB() << " ";
+            }
+            arquivo << "\n";
+        }
+
+        arquivo.close();
     }
 };
-
-int main(){
-    return 0;
-}
