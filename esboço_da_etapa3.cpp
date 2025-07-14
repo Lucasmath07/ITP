897d27a3e887263cdde388435dd7a4a76b0f58bc
Lucasmath07 <lucas.mat.ferr.roch@gmail.com>
2025-07-14 13:06:47 -0300

    Update esboço_da_etapa3.cpp

--- esboço_da_etapa3.cpp
+++ esboço_da_etapa3.cpp
@@ -2,7 +2,7 @@
 #include <random>
 using namespace std;
 
-const int N = 5;  // Tamanho da matriz (deve ser 2^n + 1 para funcionar com diamond-square)
+const int tam = 5;  // Tamanho da matriz (deve ser 2^n + 1 para funcionar com diamond-square)
 
 int aleatorio() {
     random_device rd;
@@ -12,7 +12,7 @@ int aleatorio() {
 }
 
 // Etapa diamond - calcula o centro do quadrado
-void diamond(int mat[N][N], int x1, int y1, int x2, int y2) {
+void diamond(int mat[tam][tam], int x1, int y1, int x2, int y2) {
     int cx = (x1 + x2) / 2;
     int cy = (y1 + y2) / 2;
     int media = (mat[x1][y1] + mat[x1][y2] + mat[x2][y1] + mat[x2][y2]) / 4;
@@ -20,7 +20,7 @@ void diamond(int mat[N][N], int x1, int y1, int x2, int y2) {
 }
 
 // Etapa square - calcula os centros dos lados
-void square(int mat[N][N], int x1, int y1, int x2, int y2) {
+void square(int mat[tam][tam], int x1, int y1, int x2, int y2) {
     int cx = (x1 + x2) / 2;
     int cy = (y1 + y2) / 2;
 
@@ -34,7 +34,7 @@ void square(int mat[N][N], int x1, int y1, int x2, int y2) {
     mat[cx][y2] = (mat[x1][y2] + mat[x2][y2] + mat[cx][cy]) / 3;
 }
 
-void printMatrix(int mat[N][N]) {
+void printMatrix(int mat[tam][tam]) {
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < N; j++) {
             cout.width(3);
@@ -45,7 +45,7 @@ void printMatrix(int mat[N][N]) {
 }
 
 int main() {
-    int mat[N][N] = {0};
+    int mat[tam][tam] = {0};
 
     // Define os 4 cantos com valores aleatórios
     mat[0][0] = aleatorio();
